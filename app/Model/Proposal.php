63de7de6ecdecf75d7b46e28fe609e0ee3502623
sumit kumar <sumit.rgec@gmail.com>
2017-04-23 20:10:39 +0600

    update new fie

---
+++ app/Model/Proposal.php
@@ -0,0 +1,18 @@
+<?php
+App::uses('AppModel', 'Model');
+class Proposal extends AppModel {
+   public $actsAs = array('Containable');
+   var $primaryKey = 'id'; 
+ 
+   /* public $belongsTo = array(
+		 'User'=>array(
+		  'className'=>'User',
+		  'foreignKey'=>'user_id'
+		 ),
+		 'Upload'=>array(
+		  'className'=>'Upload',
+		  'foreignKey'=>'upload_id'
+		 )
+	);  */
+  }
+?>
